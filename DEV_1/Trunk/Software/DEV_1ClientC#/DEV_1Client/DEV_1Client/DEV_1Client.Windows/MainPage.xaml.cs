7693bb9997fd101ff4c3c39741817c2f056ca9a5
Warren Woolsey <warren.woolseyiii@gmail.com>
2017-01-29 21:32:06 -0800

    Setup the firmware with the MSP430 libraries and the new device descriptors that I want to use. Setup a client side application that opens the HID device successfully. Tested the two together and got the client to open the device.

---
+++ DEV_1/Trunk/Software/DEV_1ClientC#/DEV_1Client/DEV_1Client/DEV_1Client.Windows/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Devices.Enumeration;
+using System.Threading.Tasks;
+using Windows.Devices.HumanInterfaceDevice;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DEV_1Client
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            EnumerateDevices();
+        }
+
+        static async Task EnumerateDevices()
+        {
+            ushort vId, pId, uPage, uId;
+
+            vId = 0x6969;
+            pId = 0x0001;
+            uPage = 0xFFFF;
+            uId = 0x00FF;
+
+            try
+            {
+                string selector = HidDevice.GetDeviceSelector(uPage, uId, vId, pId);
+                var devices = await DeviceInformation.FindAllAsync(selector);
+                
+                if (devices.Count > 0)
+                {
+                    string deviceIdStr = devices.ElementAt(0).Id;
+                    HidDevice device = await HidDevice.FromIdAsync(devices.ElementAt(0).Id, Windows.Storage.FileAccessMode.ReadWrite);
+                }
+            }
+            catch (Exception e0)
+            {
+                var ex = e0;
+            }
+        }
+    }
+}
