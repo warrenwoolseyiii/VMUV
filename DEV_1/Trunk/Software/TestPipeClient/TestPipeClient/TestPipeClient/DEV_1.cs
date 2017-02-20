7fb1082363618410e1bfff0dc9f65a782132698e
Warren Woolsey <warren.woolseyiii@gmail.com>
2017-02-19 17:53:54 -0800

    Async read test working

--- DEV_1/Trunk/Software/TestPipeClient/TestPipeClient/TestPipeClient/DEV_1.cs
+++ DEV_1/Trunk/Software/TestPipeClient/TestPipeClient/TestPipeClient/DEV_1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using System.IO.Pipes;
-using System;
 using DEV_1ClientConsole;
 
 namespace TestPipeClient
